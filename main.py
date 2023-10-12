17de0ca7394a795db3750c7e1776cad3f3334367
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2023-10-12 14:47:49 +0200

    Stworzenie nowych plikow i wklejenie do nich kodu. class_Note.py data_decorators.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.mongodb.db_manager import DatabaseManager
 from src.models import AdressBook, NoteBook
-import re
+from src.class_Note import *
+from src.data_decorators import *
 from datetime import datetime
-
+import re
 
 # Wszystkie zdaerzenia wykonane w tym programie maja byc zapisywane w pliku na dysku twardym.
 
@@ -21,68 +22,8 @@ from datetime import datetime
 # Chcemy miec mozliwosc usuniecia kontaktu z naszej listy.
 
 
-# Przy wprowadzaniu takich danych jak numer, telefonu, adresu e-mail czy daty urodzenia,
-#  ma wyskakiwac blad gdy dane beda nieprawidlowe.
-def validate_phone_number(func):
-    def wrapper(*args, **kwargs):
-        phone_number = args[2]
-        pattern = r"\d{9}"
-        if re.match(pattern, phone_number):
-            return func(*args, **kwargs)
-        else:
-            return f'Wrong phone number. Sample number: "123456789"'
-    return wrapper
-
-def validate_e_mail(func):
-    def wrapper(*args, **kwargs):
-        e_mail = args[3]
-        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-        if re.match(pattern, e_mail):
-            return func(*args, **kwargs)
-        else:
-            return f'Bad email. Example email address: "silmple_adres@ios.com"'
-    return wrapper
-
-def is_valid_date(func):
-    def wrapper(*args, **kwargs):
-        birthday = args[4]
-        try:
-            datetime.strptime(birthday, '%Y-%m-%d')
-            return func(*args, **kwargs)
-        except ValueError:
-            return f'Wrong date of birth. Correct format "yyyy-mm-dd"'
-    return wrapper
-
-
 # Chcemy miec mozliwosc sprawdzenia ile czy ktos ma urodziny w ciagu najblizszych 30 dni
 # badz tez wypisac liste wszystykich urodzin sortujac od najblizszych.
-
-# Druga sprawa to mozliwosc dodawanie notatek do naszego notesu.
-# Notatka ma sie skladac dwoch rzeczy: 'Tag' oraz przedmiotu notatki.
-# Przedmiotem notatki moze byc informacja tekstowa, obraz, dokument, film itp.
-# Notatki musza byc zapisywane w pliku zewnatrzym.
-class Note:
-    def __init__(self, title, tag, content):
-        self.title = title
-        self.tag = tag
-        self.content = content
-
-    def add_note(self):
-        pass
-# Dodawanie notatek do notbook.
-
-    def edit_note(self):
-        pass
-# Edycja notatek
-
-    def remove_note(self):
-        pass
-# Usuwanie notatek
-
-    def sort_note(self):
-        pass
-# Sortowanie notatek po tytule, badz tagach.
-
 
 
 # Stworzenie interfejsu poruszania sie po terminalu.
---
+++ src/data_decorators.py
@@ -0,0 +1,34 @@
+import re
+from datetime import datetime
+
+# Przy wprowadzaniu takich danych jak numer, telefonu, adresu e-mail czy daty urodzenia,
+#  ma wyskakiwac blad gdy dane beda nieprawidlowe.
+def validate_phone_number(func):
+    def wrapper(*args, **kwargs):
+        phone_number = args[2]
+        pattern = r"\d{9}"
+        if re.match(pattern, phone_number):
+            return func(*args, **kwargs)
+        else:
+            return f'Wrong phone number. Sample number: "123456789"'
+    return wrapper
+
+def validate_e_mail(func):
+    def wrapper(*args, **kwargs):
+        e_mail = args[3]
+        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        if re.match(pattern, e_mail):
+            return func(*args, **kwargs)
+        else:
+            return f'Bad email. Example email address: "silmple_adres@ios.com"'
+    return wrapper
+
+def is_valid_date(func):
+    def wrapper(*args, **kwargs):
+        birthday = args[4]
+        try:
+            datetime.strptime(birthday, '%Y-%m-%d')
+            return func(*args, **kwargs)
+        except ValueError:
+            return f'Wrong date of birth. Correct format "yyyy-mm-dd"'
+    return wrapper
