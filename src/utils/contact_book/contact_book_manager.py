7de7bfb070ec1b422d92051c6a335f7a885f1c56
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-10-13 14:38:09 +0200

    Added full CRUD to contact_book

--- src/utils/contact_book/contact_book_manager.py
+++ src/utils/contact_book/contact_book_manager.py
@@ -8,7 +8,7 @@ class ContactBookManager:
         self.db_manager = DatabaseConnectionManager()
         self.data_repo = DataRepository(self.db_manager)
 
-    def add(self, user_data):
+    def create(self, user_data):
         try:
             self.data_repo.create(AddressBook(name=user_data['name'],
                                               surname=user_data['surname'],
@@ -17,3 +17,25 @@ class ContactBookManager:
                                               birthday=user_data['birthday']))
         except Exception as e:
             print(f"An error occurred: {str(e)}")
+
+    def edit(self, field, value, updates):
+        try:
+            self.data_repo.update(value_type=AddressBook, field=field, value=value, updates=updates)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+    def delete(self, field, value):
+        try:
+            self.data_repo.delete(value_type=AddressBook, field=field, value=value)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+    def read_all(self) -> list:
+        try:
+            all_documents = self.data_repo.read_all(AddressBook)
+            return list(all_documents)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return []
+
+
