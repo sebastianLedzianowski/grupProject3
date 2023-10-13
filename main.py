18471aab129f6a0e01f0932bf2405525000348a5
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2023-10-13 13:49:46 +0200

    Poprawienie klasy NotesBook.

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ from src.mongodb.models import AddressBook, NoteBook
 def main():
     # TEST
     # Initialize the database connection and data repository
+    db_manager = DatabaseConnectionManager()
+    data_repo = DataRepository(db_manager)
 
+    for contact in data_repo.read_all(AddressBook):
+        print(f"Name: {contact['name']}, Surname: {contact['surname']}, Phone number: {contact['phone_number']},"
+              f" Email: {contact['email']}, Birthday: {contact['birthday']}\n")
+    print()
+    print("-"*130)
+    print()
+    for note in data_repo.read_all(NoteBook):
+        print(f"Title: {note['title']}, Tag: {note['tag']}\nContent: {note['content']}\n")
 
 
 if __name__ == '__main__':
--- src/class_NotesBook.py
+++ src/class_NotesBook.py
@@ -2,20 +2,25 @@ from src.mongodb.db_connection import DatabaseConnectionManager
 from src.mongodb.db_repository import DataRepository
 
 db_manager = DatabaseConnectionManager()
-data_repo = DataRepository(db_manager)
 
 class Note:
-    def __init__(self, title, tag, content):
+    def __init__(self, title, tags, content):
         self.title = title
-        self.tag = tag
+        self.tags = tags
         self.content = content
 
-class NotesBook(Note):
-    def __init__(self, title, tag, content):
-        super().__init__(title, tag, content)
-    def create(self):
-        pass
-    # Dodawanie notatek do notbook.
+class NotesBook:
+    def __init__(self):
+        self.data_repo = DataRepository(db_manager)
+
+    def input(self, title, tags, content):
+        note = Note(title, tags, content)
+        return note
+
+    def create(self, note):
+        self.data_repo.create(note)
+        print("Note created successfully.")
+
 
     def read(self):
         pass
@@ -24,6 +29,7 @@ class NotesBook(Note):
     def sort(self):
         pass
     # Sortowanie notatek po danym argumencie.
+
     def edit(self):
         pass
     # Edycja notatek
