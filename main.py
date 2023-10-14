1e7103ae7fa07328c00ce0bbd059145cf6642562
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-10-14 12:23:55 +0200

    Cleaned up main.py

--- main.py
+++ main.py
@@ -4,45 +4,10 @@ from src.mongodb.models import AddressBook, NoteBook
 from src.utils.contact_book.contact_book_collector import ContactBookCollector
 from src.utils.contact_book.contact_book_manager import ContactBookManager
 
-user_data = ContactBookCollector().get_user_input()
-ContactBookManager().add(user_data=user_data)
 
-def read():
-    # TEST
-    # Initialize the database connection and data repository
-    db_manager = DatabaseConnectionManager()
-    data_repo = DataRepository(db_manager)
-
-    for contact in data_repo.read_all(AddressBook):
-        print(f"Name: {contact['name']}, Surname: {contact['surname']}, Phone number: {contact['phone_number']},"
-              f" Email: {contact['email']}, Birthday: {contact['birthday']}\n")
-    print()
-    print("-"*130)
-    print()
-    for note in data_repo.read_all(NoteBook):
-        print(f"Title: {note['title']}, Tag: {note['tag']}\nContent: {note['content']}\n")
-
-
-# Główna funkcja obsługująca interakcję z użytkownikiem.
 def main():
-      # Inicjalizacja NotesBook oraz ContactBook
-
-    # Pętla while pozwala na ciągłe interakcje z użytkownikiem, aż do momentu zakończenia programu.
-    while True:
-    # Wyświetlanie opcji menu dla użytkownika. uzywajac ".format"
-    #     Po lewej stronie contacktBook a po prawej opcje wybory notesbook.
-    # wybory maja byc po poodaniu numeru.
-
-        # Oczekiwanie na wybór opcji od użytkownika.
-
-        # Warunki odpowiadające poszczególnym opcjom menu.
-
-        # Użytkownik może dodać, wyświetlić, sortować, edytować, usuwać lub zakończyć program.
-        # opjca edycji kontaktu oraz usunecia postepna dopiero po wybtaniu konkretnego kontaktu.
-        # Każda opcja jest obsługiwana przez odpowiednią funkcję klasy.
-
-        # Po zakończeniu wybranej operacji, program wraca do menu głównego.
+    pass
 
 
 if __name__ == '__main__':
-    read()
+    main()
