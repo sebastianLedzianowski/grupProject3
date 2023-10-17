045443a73aa2016484923959aac7a8beda40f8b9
Barbara Kulig <basia1316.bk@gmail.com>
2023-10-17 22:07:39 +0200

    Dodanie trzeciego punktu - sprawdzenie najbliższych urodzin

--- main.py
+++ main.py
@@ -291,17 +291,16 @@ def notes_menu(notes_book_manager):
 
 
 def check_birthday_menu(contact_book_manager):
-    upcoming_birthdays = contact_book_manager.get_days_to_birthday()
+     upcoming_birthdays = contact_book_manager.get_days_to_birthday()
 
-    print("\n======== Check Days Until Next Birthday ========")
-    if not upcoming_birthdays:
-        print("No upcoming birthdays found.")
-    else:
-        for birthday_info in upcoming_birthdays:
-            name = birthday_info['name']
-            surname = birthday_info['surname']
-            days_to_birthday = birthday_info['days_to_birthday']
-            print(f"{name} {surname}'s birthday is in {days_to_birthday} days.")
+     print("\n======== Check Days Until Next Birthday ========")
+     if not upcoming_birthdays:
+         print("No upcoming birthdays found.")
+     else:
+         for birthday_info in upcoming_birthdays:
+             print(f"{birthday_info['name']} {birthday_info['surname']}'s birthday is in {birthday_info['days_to_birthday']} days.")
+
+
 
 
 if __name__ == '__main__':
