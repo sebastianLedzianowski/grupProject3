6ae43b914b5be6e71e5414ada05a752bae0b03f6
Barbara Kulig <basia1316.bk@gmail.com>
2023-10-19 18:51:08 +0200

    Dodana informacja, że nie ma kontaktu w bazie Zmienione wyświetlanie duplikatów i pojedynczego kontaktu w opcji choose

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from email.message import EmailMessage
 import ssl
 from dotenv import load_dotenv
 
+
 def main():
     contact_book_manager = ContactBookManager()
     notes_book_manager = NotesBookManager()
@@ -35,6 +36,37 @@ def main():
         else:
             print("Invalid choice. Choose an option from 1 to 3.")
 
+def display_contacts(contacts):
+    if not contacts:
+        print("No contacts found.")
+    else:
+        max_name_length = max(len(contact.get('name', '')) for contact in contacts)
+        max_surname_length = max(len(contact.get('surname', '')) for contact in contacts)
+        max_phone_number_length = max(len(contact.get('phone_number', '')) for contact in contacts)
+        max_email_length = max(len(contact.get('email', '')) for contact in contacts)
+        max_birthday_length = max(len(contact.get('birthday', '')) for contact in contacts)
+        print("\n| {:^{}} | {:^{}} | {:^{}} | {:^{}} | {:^{}} |".format('Name', max_name_length, 'Surname',
+                                                                        max_surname_length, 'Phone number',
+                                                                        max_phone_number_length, 'Email',
+                                                                        max_email_length, 'Birthday',
+                                                                        max_birthday_length))
+        separator_line = "|{}+{}+{}+{}+{}|".format("-" * (max_name_length + 2), "-" * (max_surname_length + 2),
+                                                   "-" * (max_phone_number_length + 2), "-" * (max_email_length + 2),
+                                                   "-" * (max_birthday_length + 2))
+        print(separator_line)
+        for contact in contacts:
+            name = contact.get('name', '')[:max_name_length]
+            surname = contact.get('surname', '')[:max_surname_length]
+            phone_number = contact.get('phone_number', '')[:max_phone_number_length]
+            email = contact.get('email', '')[:max_email_length]
+            birthday = contact.get('birthday', '')[:max_birthday_length]
+            print("| {:^{}} | {:^{}} | {:^{}} | {:^{}} | {:^{}} |".format(name, max_name_length, surname,
+                                                                              max_surname_length, phone_number,
+                                                                              max_phone_number_length, email,
+                                                                              max_email_length, birthday,
+                                                                              max_birthday_length))
+            print(separator_line)
+
 def contact_menu(contact_book_manager):
     while True:
         print("\n===== Manage Contacts =====")
@@ -50,36 +82,7 @@ def contact_menu(contact_book_manager):
             continue
         if contact_choice == 1:
             contacts = contact_book_manager.read_all()
-            if not contacts:
-                print("No contacts found.")
-            else:
-                max_name_length = max(len(contact.get('name', '')) for contact in contacts)
-                max_surname_length = max(len(contact.get('surname', '')) for contact in contacts)
-                max_phone_number_length = max(len(contact.get('phone_number', '')) for contact in contacts)
-                max_email_length = max(len(contact.get('email', '')) for contact in contacts)
-                max_birthday_length = max(len(contact.get('birthday', '')) for contact in contacts)
-                print("\n| {:^{}} | {:^{}} | {:^{}} | {:^{}} | {:^{}} |".format('Name', max_name_length, 'Surname',
-                                                                                max_surname_length, 'Phone number',
-                                                                                max_phone_number_length, 'Email',
-                                                                                max_email_length, 'Birthday',
-                                                                                max_birthday_length))
-                separator_line = "|{}+{}+{}+{}+{}|".format("-" * (max_name_length + 2), "-" * (max_surname_length + 2),
-                                                           "-" * (max_phone_number_length + 2), "-" * (max_email_length
-                                                                                                       + 2),
-                                                           "-" * (max_birthday_length + 2))
-                print(separator_line)
-                for contact in contacts:
-                    name = contact.get('name', '')[:max_name_length]
-                    surname = contact.get('surname', '')[:max_surname_length]
-                    phone_number = contact.get('phone_number', '')[:max_phone_number_length]
-                    email = contact.get('email', '')[:max_email_length]
-                    birthday = contact.get('birthday', '')[:max_birthday_length]
-                    print("| {:^{}} | {:^{}} | {:^{}} | {:^{}} | {:^{}} |".format(name, max_name_length, surname,
-                                                                                  max_surname_length, phone_number,
-                                                                                  max_phone_number_length, email,
-                                                                                  max_email_length, birthday,
-                                                                                  max_birthday_length))
-                    print(separator_line)
+            display_contacts(contacts)
         elif contact_choice == 2:
             user_data = ContactBookCollector.get_user_input()
             contact_book_manager.create(user_data)
@@ -145,6 +148,7 @@ def contact_menu(contact_book_manager):
         else:
             print("Invalid choice. Choose an option from 1 to 6.")
 
+
 def choose_contact(contact_book_manager):
     while True:
         print("\n===== Choose Contact by =====")
@@ -168,21 +172,34 @@ def choose_contact(contact_book_manager):
             duplicates = contact_book_manager.look_for_doubles(field, value)
 
             if duplicates:
-                print("Duplicates found. Choose a duplicate to edit:")
-                for i, duplicate in enumerate(duplicates, start=1):
-                    print(f"{i}. {duplicate}")
+                if len(duplicates) == 1:
+                    print(f"Found a single contact with {field} = {value}:")
+                    display_numbered_contacts([duplicates[0]])
+                    _id = duplicates[0]["_id"]
+                    contact_edit_delete_menu(contact_book_manager, field, value, _id)
+                else:
+                    print(f"Duplicates found for {field} = {value}. Choose a duplicate to edit:")
+                    display_numbered_contacts(duplicates)
 
-                try:
-                    chosen_duplicate_index = int(input("Choose the duplicate to edit (1/2/...): ")) - 1
-                    if 0 <= chosen_duplicate_index < len(duplicates):
-                        _id = duplicates[chosen_duplicate_index]["_id"]
-                        contact_edit_delete_menu(contact_book_manager, field, value, _id)
-                    else:
-                        print("Invalid choice. No contact edited.")
-                except ValueError:
-                    print("Invalid input. Please enter a number.")
+                    try:
+                        chosen_duplicate_index = int(input("Choose the duplicate to edit (1/2/...): ")) - 1
+                        if 0 <= chosen_duplicate_index < len(duplicates):
+                            _id = duplicates[chosen_duplicate_index]["_id"]
+                            contact_edit_delete_menu(contact_book_manager, field, value, _id)
+                        else:
+                            print("Invalid choice. No contact edited.")
+                    except ValueError:
+                        print("Invalid input. Please enter a number.")
             else:
-                contact_edit_delete_menu(contact_book_manager, field, value)
+                all_contacts = contact_book_manager.read_all()
+                matching_contacts = [contact for contact in all_contacts if contact[field] == value]
+
+                if not matching_contacts:
+                    print(f"No contact found with {field} = {value}. Please choose a valid contact.")
+                else:
+                    print(f"Found a single contact with {field} = {value}:")
+                    display_numbered_contacts(matching_contacts)
+                    contact_edit_delete_menu(contact_book_manager, field, value)
             break
         elif choice == 6:
             print("Back to Choose Contact Menu.")
@@ -190,6 +207,36 @@ def choose_contact(contact_book_manager):
         else:
             print("Invalid choice. Choose an option from 1 to 6.")
 
+
+def display_numbered_contacts(contacts):
+    max_name_length = max(len(contact.get('name', '')) for contact in contacts)
+    max_surname_length = max(len(contact.get('surname', '')) for contact in contacts)
+    max_phone_number_length = max(len(contact.get('phone_number', '')) for contact in contacts)
+    max_email_length = max(len(contact.get('email', '')) for contact in contacts)
+    max_birthday_length = max(len(contact.get('birthday', '')) for contact in contacts)
+
+    print("\n| {:^5} | {:^{}} | {:^{}} | {:^{}} | {:^{}} | {:^{}} |".format(
+        'Index', 'Name', max_name_length, 'Surname', max_surname_length, 'Phone number',
+        max_phone_number_length, 'Email', max_email_length, 'Birthday', max_birthday_length))
+
+    separator_line = "|{:^7}|{}+{}+{}+{}+{}|".format(
+        ' ', "-" * (max_name_length + 2), "-" * (max_surname_length + 2),
+             "-" * (max_phone_number_length + 2), "-" * (max_email_length + 2),
+             "-" * (max_birthday_length + 2))
+
+    print(separator_line)
+
+    for i, contact in enumerate(contacts, start=1):
+        name = contact.get('name', '')[:max_name_length]
+        surname = contact.get('surname', '')[:max_surname_length]
+        phone_number = contact.get('phone_number', '')[:max_phone_number_length]
+        email = contact.get('email', '')[:max_email_length]
+        birthday = contact.get('birthday', '')[:max_birthday_length]
+        print("| {:^5} | {:^{}} | {:^{}} | {:^{}} | {:^{}} | {:^{}} |".format(
+            i, name, max_name_length, surname, max_surname_length, phone_number,
+            max_phone_number_length, email, max_email_length, birthday, max_birthday_length))
+
+        print(separator_line)
 def contact_edit_delete_menu(contact_book_manager, field, value, _id=None):
     while True:
         print("\n===== Choose option =====")
@@ -210,7 +257,7 @@ def contact_edit_delete_menu(contact_book_manager, field, value, _id=None):
         elif edit_delete_choice == 2:
             if _id != None:
                 delete_contact(contact_book_manager, "_id", _id)
-            else:    
+            else:
                 delete_contact(contact_book_manager, field, value)
             break
         elif edit_delete_choice == 3:
@@ -218,22 +265,26 @@ def contact_edit_delete_menu(contact_book_manager, field, value, _id=None):
             break
         else:
             print("Invalid choice. Choose an option from 1 to 3.")
-        
+
+
 def edit_contact(contact_book_manager, field, value):
     new_value = input(f"Enter the new value for {field}: ")
     contact_book_manager.edit(field, value, {field: new_value})
     print("Contact edited successfully.")
 
+
 def edit_contact_by_criteria(contact_book_manager, field, _id):
     new_value = input(f"Enter the new value for {field}: ")
-    search_criteria = {"_id":_id}
-    updates = {field:new_value}
+    search_criteria = {"_id": _id}
+    updates = {field: new_value}
     contact_book_manager.edit_by_criteria(search_criteria, updates)
     print("Contact edited successfully.")
+
 
 def delete_contact(contact_book_manager, field, value):
     contact_book_manager.delete(field, value)
     print("Contact deleted successfully.")
+
 
 def notes_menu(notes_book_manager):
     while True:
@@ -307,6 +358,7 @@ def notes_menu(notes_book_manager):
         else:
             print("Invalid choice. Choose an option from 1 to 6.")
 
+
 def choose_note(notes_book_manager):
     while True:
         print("\n===== Choose Note by =====")
@@ -325,7 +377,7 @@ def choose_note(notes_book_manager):
             field = fields[choice - 1]
             value = input(f"Enter the current value of the {field}: ")
             duplicates = notes_book_manager.look_for_doubles(field, value)
-    
+
             if duplicates:
                 print("Duplicates found. Choose a duplicate to edit:")
                 for i, duplicate in enumerate(duplicates, start=1):
@@ -349,6 +401,7 @@ def choose_note(notes_book_manager):
         else:
             print("Invalid choice. Choose an option from 1 to 4.")
 
+
 def note_edit_delete_menu(notes_book_manager, field, value, _id=None):
     while True:
         print("\n===== Choose option =====")
@@ -369,7 +422,7 @@ def note_edit_delete_menu(notes_book_manager, field, value, _id=None):
         elif edit_delete_choice == 2:
             if _id != None:
                 delete_contact(notes_book_manager, "_id", _id)
-            else:    
+            else:
                 delete_contact(notes_book_manager, field, value)
             break
         elif edit_delete_choice == 3:
@@ -378,21 +431,25 @@ def note_edit_delete_menu(notes_book_manager, field, value, _id=None):
         else:
             print("Invalid choice. Choose an option from 1 to 3.")
 
+
 def edit_note(notes_book_manager, field, value):
-   new_value = input(f"Enter the new value for {field}: ")
-   notes_book_manager.edit(field, value, {field: new_value})
-   print("Contact edited successfully.")
+    new_value = input(f"Enter the new value for {field}: ")
+    notes_book_manager.edit(field, value, {field: new_value})
+    print("Contact edited successfully.")
+
 
 def edit_note_by_criteria(notes_book_manager, field, _id):
     new_value = input(f"Enter the new value for {field}: ")
-    search_criteria = {"_id":_id}
-    updates = {field:new_value}
+    search_criteria = {"_id": _id}
+    updates = {field: new_value}
     notes_book_manager.edit_by_criteria(search_criteria, updates)
     print("Contact edited successfully.")
+
 
 def delete_note(notes_book_manager, field, value):
     notes_book_manager.delete(field, value)
     print("Contact deleted successfully.")
+
 
 def check_birthday_menu(contact_book_manager):
     upcoming_birthdays = contact_book_manager.get_days_to_birthday()
