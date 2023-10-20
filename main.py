e231c9af39b3d96cc20b9015295a8004ad2411f9
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2023-10-20 12:09:06 +0200

    Poprawienie nieskonczonej petli w urodzinach.

--- main.py
+++ main.py
@@ -412,20 +412,20 @@ def delete_note(notes_book_manager, field, value):
 
 
 def check_birthday_menu(contact_book_manager):
-    while True:
-        upcoming_birthdays = contact_book_manager.get_days_to_birthday
-        print("\n======== Check Days Until Next Birthday ========")
-        if not upcoming_birthdays:
-            print("No upcoming birthdays found.")
-        else:
-            for birthday_info in upcoming_birthdays:
-                print(
-                    f"{birthday_info['name']} {birthday_info['surname']}'s birthday is in "
-                    f"{birthday_info['days_to_birthday']} days.")
-            for birthday_info in upcoming_birthdays:
-                if birthday_info['days_to_birthday'] == 0:
-                    email = birthday_info['email']
-                    generate_birthday_wish(contact_book_manager, birthday_info, email)
+    upcoming_birthdays = contact_book_manager.get_days_to_birthday
+    print("\n======== Check Days Until Next Birthday ========")
+    if not upcoming_birthdays:
+        print("No upcoming birthdays found.")
+    else:
+        for birthday_info in upcoming_birthdays:
+            print(
+                f"{birthday_info['name']} {birthday_info['surname']}'s birthday is in "
+                f"{birthday_info['days_to_birthday']} days.")
+
+        for birthday_info in upcoming_birthdays:
+            if birthday_info['days_to_birthday'] == 0:
+                email = birthday_info['email']
+                generate_birthday_wish(contact_book_manager, birthday_info, email)
 
 
 def generate_birthday_wish(contact_book_manager, name, email):
@@ -433,7 +433,6 @@ def generate_birthday_wish(contact_book_manager, name, email):
         print(f"\nWould you like to generate birthday wishes {name['name']}?")
         print("1. Yes")
         print("2. No")
-        print('3. Back to Main Menu')
         user_choice = str(input("Choose option (1/2): "))
         if user_choice == '1':
             response = contact_book_manager.get_birthday_wish(name)
@@ -447,8 +446,6 @@ def generate_birthday_wish(contact_book_manager, name, email):
         elif user_choice == '2':
             print(f"Birthday wishes not generated.")
             return
-        elif user_choice == '3':
-            main()
         else:
             print("Invalid choice. Birthday wishes not generated.")
 
