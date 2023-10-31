2d944eff3b6d639c747b0256674cecf1a173bdd2
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2023-10-31 20:26:43 +0100

    Uncommenting a line in a test_send_sms.py

--- tests/test_send_sms.py
+++ tests/test_send_sms.py
@@ -1,9 +1,9 @@
-# from src.utils.sms_api import *
-#
-# def test_send_sms():
-#     # phone_number = "781087866"
-#     # text_message = "To wiadomosc testowa przy pomocy test_send_sms"
-#     sms_api(phone_number, text_message)
-#
-# if __name__ == "__main__":
-#     test_send_sms()
+from src.utils.sms_api import *
+
+def test_send_sms():
+    phone_number = "781087866"
+    text_message = "To wiadomosc testowa przy pomocy test_send_sms"
+    sms_api(phone_number, text_message)
+
+if __name__ == "__main__":
+    test_send_sms()
